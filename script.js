fb6fd10ee8afe9285ca28d78b9730bb239856254
anhld <wolfwithcode@gmail.com>
2021-11-23 16:06:27 +0700

    add form in bootstrap style

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const $ = document.querySelector.bind(document);
+
+const app = (() => {
+  const submitBtm = $("#submit");
+
+  return {
+    init() {
+      submitBtm.onclick = () => {
+        alert("test");
+      };
+    },
+  };
+})();
+
+app.init();
