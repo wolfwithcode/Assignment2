3dcf17d3b4ca8dbbb73696bce856e37d6f7b7632
anhld <wolfwithcode@gmail.com>
2021-11-23 17:43:29 +0700

    add bootstrap

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const $ = document.querySelector.bind(document);
+const messageVi = "Số điện thoại không hợp lệ!";
+const messageEn = "Invalid international phone number!";
 
 const app = (() => {
   const submitBtm = $("#submit");
+  const inputPhone = $("#inputPhone");
+
+  function isPhone(phone) {
+    console.log(phone);
+    const result = /^\+(?:[0-9] ?){6,14}[0-9]$/.test(phone);
+    console.log(result);
+    return result;
+  }
 
   return {
     init() {
       submitBtm.onclick = () => {
-        alert("test");
+        var language =
+          window.navigator.userLanguage || window.navigator.language;
+
+        if (!isPhone(inputPhone.value)) {
+          let message = language === "vi" ? messageVi : messageEn;
+          alert(`${message} : ${inputPhone.value}`);
+        }
       };
     },
   };
